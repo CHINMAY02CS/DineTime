25b053b31d877bdd40dcbff28d540101257fa1d1
CHINMAY02CS <dpsvn.chin06162@gmail.com>
2025-07-07 23:52:03 +0530

    Signup page UI completed
    Added signup signin and guest login buttons with logo and image

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,61 @@
 import { useRouter } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const LogoImage = require("@/assets/images/dinetimelogo.png");
+const FrameImage = require("@/assets/images/Frame.png");
 
 export default function Index() {
   const router = useRouter();
   return (
-    <View className="bg-blue-50 flex-1 items-center justify-center">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <TouchableOpacity onPress={() => router.navigate("/home")}>
-        <Text>Go to tabs</Text>
-      </TouchableOpacity>
-    </View>
+    <SafeAreaView className={`bg-[#155E95]`}>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="m-2 flex items-center justify-center">
+          <Image className="h-72 w-72" source={LogoImage} />
+          <View className="w-3/4">
+            <TouchableOpacity
+              className="p-2 my-2 bg-[#FFD66B] rounded-lg"
+              onPress={() => router.push("/signup")}
+            >
+              <Text className="text-base font-semibold text-center">
+                Sign Up
+              </Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              className="p-2 my-2 border-[#FFD66B] border rounded-lg max-w-fit"
+              onPress={() => router.push("/home")}
+            >
+              <Text className="text-base font-semibold text-center text-white">
+                Guest User
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View>
+            <Text className="text-center text-base text-white my-4 font-semibold">
+              <View className="border-b-2 border-b-[#FFD66B] p-2 mb-1 w-24" />{" "}
+              or {""}
+              <View className="border-b-2 border-b-[#FFD66B] p-2 mb-1 w-24" />
+            </Text>
+            <TouchableOpacity
+              className="p-2 my-2 rounded-lg flex items-center justify-center flex-row gap-2"
+              onPress={() => router.push("/signin")}
+            >
+              <Text className="text-base font-semibold text-center text-white">
+                Already a user?
+              </Text>
+              <Text className="text-base font-semibold text-center text-[#FFD66B] underline">
+                Sign In
+              </Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View className="flex-1">
+          <Image
+            className="h-full w-full"
+            resizeMode="contain"
+            source={FrameImage}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
